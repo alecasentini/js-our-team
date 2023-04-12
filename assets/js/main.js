fefcbfa752b0e136e1e94c5914268d70e3a704fe
alecasentini <ale.casentini@gmail.com>
2023-04-12 15:06:18 +0200

    Aggiunta stampa in card degli elementi - Esercizio completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,31 @@ const team = [
       image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+  const teamContainer = document.querySelector('.container');
+
+  const row1 = document.createElement('div');
+  row1.classList.add('row', 'justify-content-center', 'gap-3', 'py-3');
+  
+  const row2 = document.createElement('div');
+  row2.classList.add('row', 'justify-content-center', 'gap-3', 'py-3');
+  
+  teamContainer.appendChild(row1);
+  teamContainer.appendChild(row2);
+  
+  for (let i = 0; i < team.length; i++) {
+  const member = team[i];
+  const card = '<div class="card col-4" style="width: 18rem;">' +
+               '<img src="./assets/img/' + member.image + '" class="card-img-top" alt="...">' +
+               '<div class="card-body text-center">' +
+               '<h5 class="card-title name">' + member.name + '</h5>' +
+               '<p class="card-text role">' + member.role + '</p>' +
+               '</div>' +
+               '</div>';
+
+  if (i < 3) {
+      row1.innerHTML += card;
+  } else {
+      row2.innerHTML += card;
+  }
+}
